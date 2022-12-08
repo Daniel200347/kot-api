f1e68446e15e98be2a51f7655259a9b31bf5e73a
Султанов <daniel200347@mail.ru>
2022-12-09 02:00:48 +0300

    kot preloader

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById(1).onclick = print;
+
+async function print() {
+    const response = await fetch("https://aws.random.cat/meow")
+    const resultFetch = await response.json();
+    document.getElementById("image").src = resultFetch.file;
+}
+
+print();
