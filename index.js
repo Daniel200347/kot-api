1a79489352f4afd6cb8c8a8c23f0d7a1ff720ade
Султанов Даниэль <daniel200347@mail.ru>
2023-06-03 06:14:12 +0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 document.getElementById(1).onclick = print;
 
 async function print() {
-    const response = await fetch("https://aws.random.cat/meow")
+    const url = `https://api.thecatapi.com/v1/images/search`;
+    const api_key = "DEMO_API_KEY"
+
+    const response = await fetch(url, {headers: {'x-api-key': api_key}})
     const resultFetch = await response.json();
-    document.getElementById("image").src = resultFetch.file;
+
+    document.getElementById("image").src = resultFetch[0].url;
+
 }
+
 print();
